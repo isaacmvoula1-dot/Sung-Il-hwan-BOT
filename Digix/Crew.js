ca72c3019043472f219a6e4db764a9d4bba881d9
isaacmvoula1-dot <isaacmvoula1@gmail.com>
2026-02-20 18:52:49 +0100

    Update Crew.js

--- Digix/Crew.js
+++ Digix/Crew.js
@@ -14,7 +14,7 @@ async function getUserNumber() {
             output: process.stdout,
         });
 
-        rl.question('📲 Enter your WhatsApp number (with country code, e.g., 243xxxx): ', (number) => {
+        rl.question('📲 Enter your WhatsApp number (with country code, e.g., 242xxxx): ', (number) => {
             rl.close();
             resolve(number.trim());
         });
